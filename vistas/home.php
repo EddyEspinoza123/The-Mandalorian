fa7cac1de4c522014dddae219abfa963e64a1212
Eddy Espinoza <josujrth@gmail.com>
2022-07-18 16:02:04 -0500

    The Mandalorian

---
+++ vistas/home.php
@@ -0,0 +1,6 @@
+<div class="container is-fluid">
+	<h1 class="title">Home</h1>
+	<h2 class="subtitle">¡Bienvenido al portal, <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
+	<center><img src="./img/Mandalorian-Colors.png" width="600" height="500"></center>
+	
+</div>
